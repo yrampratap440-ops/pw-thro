65e72a9cb2700d8aacd710d3f4a5d825e78b4ae2
yrampratap440-ops <yrampratap440@gmail.com>
2026-08-20 17:52:24 +0530

    Update access.tsx

--- artifacts/pw-clone/src/pages/access.tsx
+++ artifacts/pw-clone/src/pages/access.tsx
@@ -4,7 +4,7 @@ import { generateAndRedirect, prepareAccessGeneration, storePendingGeneration, s
 import { useEffect, useRef, useState } from "react";
 import { useLocation } from "wouter";
 
-const AROLINKS_URL = "https://arolinks.com/QuU";
+const AROLINKS_URL = "https://arolinks.com/QYk";
 
 const styles = {
   shell: {
